4b1c0954f1328da83f7d23240defd6a77e6e4604
huntercordes <huntercordes@gmail.com>
2025-09-27 21:39:08 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
 export default App;
 */
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import MyWork from "./pages/MyWork";
